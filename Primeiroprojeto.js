10d0f257495566e97f8d8e49087d9493f6d87802
gubrbosa192-png <gubarbosa192@gmail.com>
2026-02-18 19:33:38 -0300

    segundo commit

--- Primeiroprojeto.js
+++ Primeiroprojeto.js
@@ -1,1 +1,32 @@
-console.log("Olá, mundo!")
+// Classificar nivel do herói de acordo com a experiência (xp)
+let heroi = "Guerreiro"
+let  xp = 1000000
+let level = ""
+
+if (xp <= 0) {
+  console.log("Invalido, o nivel não pode ser menor ou igual a 0.")
+  return
+}
+// Criar uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+if (xp <= 1000) {
+  level = "Ferro"
+} else if (xp >= 1001 && xp < 2000) {
+  level = "Bronze"
+} else if (xp >= 2001 && xp < 5000) {
+  level = "Prata"
+} else if (xp >= 5001 && xp < 7000) {
+  level = "Ouro"
+} else if (xp >= 7001 && xp < 8000) {
+  level = "Platina"
+} else if (xp >= 8001 && xp < 9000) {
+  level = "Ascendente"
+} else if (xp >= 9001 && xp < 10000) {
+  level = "Imortal"
+} else {
+  level = "Radiante"
+} 
+
+
+console.log("O nosso " + heroi + " está no nível " + level )
+
